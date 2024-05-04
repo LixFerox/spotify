b47a8c7db475c7e02792a197be750b68ab87e1cf
LixFerox <144841907+LixFerox@users.noreply.github.com>
2024-05-04 16:48:41 +0200

    Delete Spotify directory

--- Spotify/src/scripts/forwarding.js
+++
@@ -1,5 +0,0 @@
-let url = new URL(window.location.href);
-let param = url.searchParams.get("user");
-if (param == null) {
-  window.location.href = "login.html";
-}
--- Spotify/src/scripts/username.js
+++
@@ -1,8 +0,0 @@
-function changeUser() {
-  let url = new URL(window.location.href);
-  let username = url.searchParams.get("user");
-  let changeuser = (document.getElementById("greeting").innerHTML =
-    "Bienvenido de nuevo " + username);
-  let changesession = (document.getElementById("sessionuser").innerHTML =
-    username);
-}
